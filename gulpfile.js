d992cc747ceefcf51ac1ca0129d6466628f711e7
Ali Padron <alijesus29@gmail.com>
2019-06-28 23:38:18 -0400

    Gulp and PostCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,31 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch');
+var gulp = require("gulp"),
+  watch = require("gulp-watch"),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  cssvars = require('postcss-simple-vars'),
+  nested = require('postcss-nested');
 
-gulp.task('default', function() {
-  console.log('Hooray - you created a Gulp task.');
+gulp.task("default", function() {
+  console.log("Hooray - you created a Gulp task.");
 });
 
-gulp.task('html', function() {
-  console.log('Imagine something useful being done to your HTML here.');
+gulp.task("html", function() {
+  console.log("Imagine something useful being done to your HTML here.");
 });
 
-gulp.task('styles', function() {
-  console.log('Imagine Sass or PostCSS tasks running here.');
+gulp.task("styles", function() {
+  return gulp
+    .src(__dirname + "/app/assets/styles/styles.css")
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest(__dirname + "/app/temp/styles"));
 });
 
-gulp.task('watch', function() {
-  watch(__dirname + '/app/index.html', function() {
-    gulp.start('html');
-  })
-
-  watch(__dirname + '/app/assets/styles/**/*.css', function() {
-    gulp.start('styles');
+gulp.task("watch", function() {
+  watch(__dirname + "/app/index.html", function() {
+    gulp.start("html");
   });
-})
+
+  watch(__dirname + "/app/assets/styles/**/*.css", function() {
+    gulp.start("styles");
+  });
+});
