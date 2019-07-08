b6441f55a187cb9ba90c72e4f65bac711eeec1df
Ali Padron <alijesus29@gmail.com>
2019-07-08 16:50:48 -0400

    How to Avoid jQuery Spaghetti

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,19 +1,3 @@
-var $ = require('jquery');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + ' now owes $0 in taxes.');
-  }
-}
-
-alert('ASDASSAD');
-
-var ali = new Person('Ali Padrón', 'orange');
-ali.greet();
-
-var isabel = new Adult('Isabel Argüelles', 'green');
-isabel.greet();
-isabel.payTaxes();
-
-$('h1').remove();
+var mobileMenu = new MobileMenu();
