77590ea54f36987916318cd1dcc0b37fe9b4a3f9
Ali Padron <alijesus29@gmail.com>
2019-07-12 11:18:29 -0400

    Reavealing Elements on scroll

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,27 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+export default class RevealOnScroll {
+  constructor() {
+    this.itemsToReveal = $(".feature-item");
+    this.hideInitially();
+    this.createWaypoints();
+  }
+
+  hideInitially() {
+    this.itemsToReveal.addClass('reveal-item');
+  }
+
+  createWaypoints() {
+    this.itemsToReveal.each(function() {
+      var currentItem = this;
+      new Waypoint({
+        element: currentItem,
+        handler: function() {
+          $(currentItem).addClass('reveal-item--is-visible');
+        },
+        offset: "85%"
+      })
+    });
+  }
+}
